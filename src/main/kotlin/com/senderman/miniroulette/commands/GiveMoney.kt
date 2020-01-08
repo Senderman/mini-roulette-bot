63f79c291c2b7a1633a80c956b181484fc03485a
Fyodor Doletov <doletov.fyodor@yandex.ru>
2020-01-08 14:30:26 +0300

    fix -

--- src/main/kotlin/com/senderman/miniroulette/commands/GiveMoney.kt
+++ src/main/kotlin/com/senderman/miniroulette/commands/GiveMoney.kt
@@ -12,8 +12,7 @@ class GiveMoney(private val handler: RouletteBotHandler) : CommandExecutor {
         get() = "(reply) дать монетки. формат /give x"
 
     override fun execute(message: Message) {
-        if (!message.isReply)
-            return
+        if (!message.isReply) return
 
         val chatId = message.chatId
         val amount = try {
@@ -21,17 +20,23 @@ class GiveMoney(private val handler: RouletteBotHandler) : CommandExecutor {
         } catch (e: NumberFormatException) {
             return
         }
-        val sender = message.from.id
-        val receiver = message.replyToMessage.from.id
-        if (Services.db.getCoins(sender) - 10 < amount) {
+        if (amount < 1) return
+
+        val sender = message.from
+        val receiver = message.replyToMessage.from
+        if (receiver.bot){
+            handler.sendMessage(chatId, "Но это же бот!", message.messageId)
+            return
+        }
+        if (Services.db.getCoins(sender.id) - 10 < amount) {
             handler.sendMessage(
                 chatId, "У вас недостаточно денег (на балансе должно остаться минимум 10 монет!",
                 message.messageId
             )
             return
         }
-        Services.db.takeCoins(sender, amount)
-        Services.db.addCoins(receiver, amount)
+        Services.db.takeCoins(sender.id, amount)
+        Services.db.addCoins(receiver.id, amount)
         handler.sendMessage(chatId, "✅ Успешно!", message.messageId)
     }
 }
