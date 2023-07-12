efa84090c54a4229388738cc3f3271a0ec4034f4
Fyodor Doletov <doletov.fyodor@yandex.ru>
2023-07-12 21:28:17 +0300

    [core] start bot on Startup Event

--- src/main/java/com/senderman/miniroulette/MiniRouletteBot.java
+++ src/main/java/com/senderman/miniroulette/MiniRouletteBot.java
@@ -4,26 +4,20 @@ import com.annimon.tgbotsmodule.BotHandler;
 import com.annimon.tgbotsmodule.BotModule;
 import com.annimon.tgbotsmodule.Runner;
 import com.annimon.tgbotsmodule.beans.Config;
-import io.micronaut.context.ApplicationContext;
-import io.micronaut.runtime.ApplicationConfiguration;
-import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.context.event.StartupEvent;
 import io.micronaut.runtime.Micronaut;
-import io.micronaut.scheduling.annotation.Scheduled;
+import io.micronaut.runtime.event.annotation.EventListener;
 import jakarta.inject.Singleton;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
 @Singleton
-public class MiniRouletteBot implements EmbeddedApplication<MiniRouletteBot>, BotModule {
+public class MiniRouletteBot implements BotModule {
 
-    private final ApplicationContext context;
-    private final ApplicationConfiguration configuration;
     private final BotHandler botHandler;
 
-    public MiniRouletteBot(ApplicationContext context, ApplicationConfiguration configuration, BotHandler botHandler) {
-        this.context = context;
-        this.configuration = configuration;
+    public MiniRouletteBot(BotHandler botHandler) {
         this.botHandler = botHandler;
     }
 
@@ -44,34 +38,14 @@ public class MiniRouletteBot implements EmbeddedApplication<MiniRouletteBot>, Bo
                 .start();
     }
 
-    @Scheduled(initialDelay = "1s")
-    protected void runBot() {
-        new Thread(() -> Runner.run(List.of(this))).start();
+    @EventListener
+    protected void run(StartupEvent event) {
+        Runner.run(List.of(this));
     }
 
     @Override
     public @NotNull BotHandler botHandler(@NotNull Config config) {
         return botHandler;
-    }
-
-    @Override
-    public ApplicationContext getApplicationContext() {
-        return context;
-    }
-
-    @Override
-    public ApplicationConfiguration getApplicationConfiguration() {
-        return configuration;
-    }
-
-    @Override
-    public boolean isServer() {
-        return true;
-    }
-
-    @Override
-    public boolean isRunning() {
-        return true;
     }
 
 
